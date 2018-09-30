f371e32de716070676a121757357ff081ab37af3
Jack_McKing <JackMcKing@users.noreply.github.com>
2018-09-30 12:06:11 +0800

    Delete mmm.py

--- src/app/mmm.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-
-os.chdir('C:\\Users\\chenguwa\\PycharmProjects\\KitchenAPI\\src')
-sys.path.append('C:\\Users\\chenguwa\\PycharmProjects\\KitchenAPI\\src')
-
-from sql_handler.SQL_handler import SQL_handler
-
-s = SQL_handler(path='C:\\Users\\chenguwa\\PycharmProjects\\KitchenAPI\\src\\resource\\links.sql')
-s.readdata()
